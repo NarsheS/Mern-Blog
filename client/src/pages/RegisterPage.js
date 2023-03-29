30d07395141a4db56415e5e12811afc72d864541
NarsheS <shadownightmaresnw@gmail.com>
2023-03-29 19:05:33 -0300

    0

---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(event){
+    event.preventDefault();
+    const response = await fetch('http://localhost:4000/register', {
+      method: "POST",
+      body: JSON.stringify({username, password}),
+      headers: {"Content-Type": "application/json"},
+    });
+    if (response.status === 200){
+      alert("Registration Successful");
+    } else {
+      alert("Registration failed");
+    }
+  }
+
+  return (
+    <form className='register' action="" onSubmit={register}>
+        <h1>Register</h1>
+        <input type="text" placeholder='username' value={username} onChange={e => setUsername(e.target.value)} />
+        <input type="password" placeholder='password' value={password} onChange={e => setPassword(e.target.value)} />
+        <button>Login</button>
+    </form>
+  )
+}
+
+export default RegisterPage
